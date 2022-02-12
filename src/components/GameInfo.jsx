64dc4284bcb0676c06cf3323e526b272319febbb
tangjm <jaredtang@hotmail.co.uk>
2022-02-12 14:20:40 +0000

    remove comments from GameInfo component

--- src/components/GameInfo.jsx
+++ src/components/GameInfo.jsx
@@ -65,14 +65,6 @@ const GameInfo = ({ gameInfo }) => {
 			}
 		}
 
-		// A3, B3, C3
-		// A2, B2, C2, 
-		// A1, B1, C1
-
-		// A0, A1, A2,
-		// B0, B1, B2,
-		// C0, C1, C2
-
 		const coordinates = [];
 		for (let i = 3; i > 0; i--) {
 			for (let j = 0; j < 3; j++) {
